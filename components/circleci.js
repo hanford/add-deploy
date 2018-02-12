72a4d6031deb51944666630d88b48fb7c95ebc38
hanford <jackhanford@gmail.com>
2018-02-12 14:20:26 -0800

    fix file creation

--- components/circleci.js
+++ components/circleci.js
@@ -8,7 +8,7 @@ function CircleCi (dir, name) {
   const file = path.join(dir, 'circle.yml')
 
   const body = fs
-    .readFileSync(path.resolve(dir, './circle-template.yml'), 'utf-8')
+    .readFileSync(path.resolve(__dirname, './circle-template.yml'), 'utf-8')
 
   return fs.writeFileSync(file, body)
 }
