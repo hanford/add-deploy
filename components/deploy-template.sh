2e006568541fc1ac2de2047c1c7a43b17b84df97
hanford <jackhanford@gmail.com>
2018-02-12 14:09:48 -0800

    move now_feature into flag

--- components/deploy-template.sh
+++ components/deploy-template.sh
@@ -6,12 +6,7 @@ PRODUCTION=eazecom-production
 
 if [[ "$ENV" == "feature" ]]; then
   # now_feature
-  echo Deploying to now.sh
-  FEATURE_ENV=`now -t $NOW_TOKEN --team eaze -e COMMIT_SHA=$CIRCLE_SHA1`
-  printf "\n" # print new line!
-  echo Environment: $FEATURE_ENV
-  echo Begin runnin bundle-cop
-  github --comment 'Now Environment' --link $FEATURE_ENV
+
   # bundle-cop --branch origin/master --circleci true --link $FEATURE_ENV
 fi
 
