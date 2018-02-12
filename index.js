42f00a7acb7c0457eba50afabdea80938c79f9f2
hanford <jackhanford@gmail.com>
2018-02-12 14:16:44 -0800

    remove mkdirp

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const path = require('path')
 const fs = require('fs')
-const mkdirp = require('mkdirp')
 const chalk = require('chalk')
 const { version } = require('./package.json')
 const { cwd } = process
